14369191f85ff395c35e49d1475d7693008b6948
Takeru Ohta <phjgt308@gmail.com>
2024-12-29 20:16:16 +0900

    Add basic rendering

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,50 @@
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
 use orfail::OrFail;
 
-use crate::terminal::Terminal;
+use crate::{
+    diff::Diff,
+    git::Git,
+    terminal::{Canvas, Terminal, Text},
+};
 
 #[derive(Debug)]
 pub struct App {
     terminal: Terminal,
     exit: bool,
+    git: Git,
+    unstaged_diff: Diff,
+    staged_diff: Diff,
 }
 
 impl App {
     pub fn new() -> orfail::Result<Self> {
+        let git = Git::new();
+        let unstaged_diff = git.diff().or_fail()?;
+        let staged_diff = git.diff_cached().or_fail()?;
+
         let terminal = Terminal::new().or_fail()?;
         Ok(Self {
             terminal,
             exit: false,
+            git,
+            unstaged_diff,
+            staged_diff,
         })
     }
 
     pub fn run(mut self) -> orfail::Result<()> {
+        self.render().or_fail()?;
         while !self.exit {
             let event = self.terminal.next_event().or_fail()?;
             self.handle_event(event).or_fail()?;
         }
+        Ok(())
+    }
+
+    fn render(&mut self) -> orfail::Result<()> {
+        let mut canvas = Canvas::new();
+        canvas.draw_text(Text::new("Hello").or_fail()?);
+        self.terminal.render(canvas).or_fail()?;
         Ok(())
     }
 
@@ -33,7 +55,10 @@ impl App {
             Event::Key(event) => self.handle_key_event(event).or_fail(),
             Event::Mouse(_) => Ok(()),
             Event::Paste(_) => Ok(()),
-            Event::Resize(_, _) => todo!(),
+            Event::Resize(_, _) => {
+                self.terminal.on_resized().or_fail()?;
+                self.render().or_fail()
+            }
         }
     }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{io::Write, time::Duration};
 
 use crossterm::{
     event::Event,
@@ -7,13 +7,74 @@ use crossterm::{
 use orfail::OrFail;
 
 #[derive(Debug)]
-pub struct Terminal {}
+pub struct Text {
+    text: String,
+}
+
+impl Text {
+    pub fn new(text: &str) -> orfail::Result<Self> {
+        // TODO: validate
+        Ok(Self {
+            text: text.to_owned(),
+        })
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Row {
+    pub texts: Vec<Text>,
+}
+
+// TODO: rename
+#[derive(Debug)]
+pub struct Canvas {
+    current_row: Row,
+    rows: Vec<Row>,
+}
+
+impl Canvas {
+    pub fn new() -> Self {
+        Self {
+            current_row: Row::default(),
+            rows: Vec::new(),
+        }
+    }
+
+    pub fn draw_text(&mut self, text: Text) {
+        self.current_row.texts.push(text);
+    }
+
+    pub fn draw_newline(&mut self) {
+        let last_row = std::mem::take(&mut self.current_row);
+        self.rows.push(last_row);
+    }
+
+    pub fn rows(&self) -> usize {
+        self.rows.len() + 1
+    }
+}
+
+#[derive(Debug)]
+pub struct Terminal {
+    size: Size,
+}
 
 impl Terminal {
     pub fn new() -> orfail::Result<Self> {
         crossterm::execute!(std::io::stdout(), EnterAlternateScreen).or_fail()?;
         crossterm::terminal::enable_raw_mode().or_fail()?;
-        Ok(Self {})
+
+        let size = Size::current().or_fail()?;
+        Ok(Self { size })
+    }
+
+    pub fn size(&self) -> Size {
+        self.size
+    }
+
+    pub fn on_resized(&mut self) -> orfail::Result<()> {
+        self.size = Size::current().or_fail()?;
+        Ok(())
     }
 
     pub fn next_event(&mut self) -> orfail::Result<Event> {
@@ -21,11 +82,64 @@ impl Terminal {
         while !crossterm::event::poll(timeout).or_fail()? {}
         crossterm::event::read().or_fail()
     }
+
+    pub fn render(&mut self, mut canvas: Canvas) -> orfail::Result<()> {
+        canvas.draw_newline(); // TODO
+
+        let stdout = std::io::stdout();
+        let mut writer = stdout.lock();
+        crossterm::queue!(
+            writer,
+            crossterm::cursor::SavePosition,
+            crossterm::terminal::Clear(crossterm::terminal::ClearType::All)
+        )
+        .or_fail()?;
+
+        for (row_i, row) in canvas.rows.into_iter().enumerate() {
+            crossterm::queue!(writer, crossterm::cursor::MoveTo(0, row_i as u16)).or_fail()?;
+
+            for text in row.texts {
+                crossterm::queue!(writer, crossterm::style::Print(text.text)).or_fail()?;
+            }
+        }
+
+        crossterm::queue!(writer, crossterm::cursor::RestorePosition,).or_fail()?;
+        writer.flush().or_fail()?;
+        Ok(())
+    }
 }
 
 impl Drop for Terminal {
     fn drop(&mut self) {
         let _ = crossterm::terminal::disable_raw_mode();
         let _ = crossterm::execute!(std::io::stdout(), LeaveAlternateScreen);
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Size {
+    pub rows: usize,
+    pub cols: usize,
+}
+
+impl Size {
+    pub fn current() -> orfail::Result<Self> {
+        let size = crossterm::terminal::size().or_fail()?;
+        Ok(Self {
+            rows: size.1 as usize,
+            cols: size.0 as usize,
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Position {
+    pub row: usize,
+    pub col: usize,
+}
+
+impl Position {
+    pub fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
     }
 }
