4ba6d682300d0c4319792963af4ec8f6c1b298dd
Takeru Ohta <phjgt308@gmail.com>
2024-12-29 17:47:06 +0900

    Remove clap

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use clap::Parser;
-use mamediff::{app::App, git::Git, terminal::Terminal};
+use mamediff::app::App;
 use orfail::OrFail;
 
-#[derive(Parser)]
-#[clap(version)]
-struct Args {}
-
 fn main() -> orfail::Result<()> {
-    let _args = Args::parse();
     let app = App::new().or_fail()?;
     app.run().or_fail()?;
     Ok(())
