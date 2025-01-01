e6942f508e4a6c255899e94acb388cd00ec8e9ed
Takeru Ohta <phjgt308@gmail.com>
2025-01-01 12:06:43 +0900

    Add Git::stage()

--- src/git.rs
+++ src/git.rs
@@ -13,10 +13,39 @@ impl Git {
     }
 
     pub fn stage(&self, diff: &Diff) -> orfail::Result<()> {
-        todo!()
+        let patch = diff.to_string();
+
+        // TODO: use pipe
+        std::fs::write(".mamediff.patch", &patch).or_fail()?;
+
+        let output = Command::new("git")
+            .arg("apply")
+            .arg("--cached")
+            .arg(".mamediff.patch") // TODO: use pipe
+            .output()
+            .or_fail_with(|e| format!("Failed to execute `$ git apply --cached`: {e}"))?;
+        output.status.success().or_fail_with(|()| {
+            format!(
+                "Failed to execute `$ git apply --cached`{}{}",
+                output
+                    .status
+                    .code()
+                    .map(|c| format!(": exit_code={c}"))
+                    .unwrap_or_default(),
+                (!output.stderr.is_empty())
+                    .then(|| format!(
+                        "\n\nSTDERR\n------\n{}\n------",
+                        String::from_utf8_lossy(&output.stderr)
+                    ))
+                    .unwrap_or_default()
+            )
+        })?;
+
+        Ok(())
     }
 
     pub fn diff(&self) -> orfail::Result<Diff> {
+        // TODO: factor out
         let output = Command::new("git")
             .arg("diff")
             .output()
