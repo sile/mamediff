aca20eb4be876346d8a961da19a37095d0c8bbe4
Takeru Ohta <phjgt308@gmail.com>
2025-01-05 11:19:43 +0900

    Add TODO comment

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use mamediff::app::App;
 use orfail::OrFail;
 
 fn main() -> orfail::Result<()> {
+    // TIDO: Add -h option
     let app = App::new().or_fail()?;
     app.run().or_fail()?;
     Ok(())
