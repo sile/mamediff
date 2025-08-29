88fc4f4137a2aabe488270ed6e1d408e24b433b1
Takeru Ohta <phjgt308@gmail.com>
2025-08-29 09:01:55 +0900

    Rename cursor movement actions from cursor-* to move-*

--- src/action.rs
+++ src/action.rs
@@ -2,10 +2,10 @@
 pub enum Action {
     Quit,
     Recenter,
-    CursorUp,
-    CursorDown,
-    CursorLeft,
-    CursorRight,
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
     Toggle,
     Stage,
     Discard,
@@ -29,17 +29,20 @@ impl<'text, 'raw> TryFrom<nojson::RawJsonValue<'text, 'raw>> for Action {
         match ty.to_unquoted_string_str()?.as_ref() {
             "quit" => Ok(Self::Quit),
             "recenter" => Ok(Self::Recenter),
-            "cursor-up" => Ok(Self::CursorUp),
-            "cursor-down" => Ok(Self::CursorDown),
-            "cursor-left" => Ok(Self::CursorLeft),
-            "cursor-right" => Ok(Self::CursorRight),
+            "move-up" => Ok(Self::MoveUp),
+            "move-down" => Ok(Self::MoveDown),
+            "move-left" => Ok(Self::MoveLeft),
+            "move-right" => Ok(Self::MoveRight),
             "toggle" => Ok(Self::Toggle),
             "stage" => Ok(Self::Stage),
             "discard" => Ok(Self::Discard),
             "unstage" => Ok(Self::Unstage),
             "toggle-legend" => Ok(Self::ToggleLegend),
             "init-legend" => {
-                let hide = value.to_member("hide")?.map(bool::try_from)?.unwrap_or_default();
+                let hide = value
+                    .to_member("hide")?
+                    .map(bool::try_from)?
+                    .unwrap_or_default();
                 let labels = value.to_member("labels")?.required()?;
                 let label_show = labels.to_member("show")?.required()?.try_into()?;
                 let label_hide = labels.to_member("hide")?.required()?.try_into()?;
